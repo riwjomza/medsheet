920c257acbbcc8a1d154b07257e43d1fe693e6f6
Sawettachat <Sawettachat_suthum@cmu.ac.th>
2023-01-07 00:49:36 +0700

    Auth_ Demo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layouts'
+import {AuthProvider} from '../context/AuthContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <AuthProvider>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </AuthProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
+import { useAuth } from '../context/AuthContext'
+import Login from './login'
 
 export default function Home() {
+  const {currentUser} =useAuth()
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <Image src={'/img/logo.png'} alt="" width={100} height={100} />
